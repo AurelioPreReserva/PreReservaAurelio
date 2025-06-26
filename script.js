cebd69cef2c1256adb5d5737c84916981904dfea
AurelioPreReserva <aurelioprereserva@gmail.com>
2025-06-26 14:38:47 -0300

    pruebanueva

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     // ESTA ES LA URL DE TU GOOGLE APPS SCRIPT
     // ¡REEMPLAZA "TU_URL_DE_APPS_SCRIPT_AQUI" CON LA URL REAL DE TU DEPLOYMENT!
-   const appsScriptURL = "https://script.google.com/macros/s/AKfycby175eIcbn5E0IfAbVIIDQKaHzX2-Vdh-vDqxjusyia0MYm5xI8EMkdzcos0zlxGOw/exec";
+   const appsScriptURL = "https://script.google.com/macros/s/AKfycbzrCzIoSm5-7HTQ0XlAlbH6jLr21S4NVPuPkePO0J_HQ2B-cD2kkuh4TCgFE15-MSlr/exec";
 
     // Set today's date
     const today = new Date().toISOString().slice(0, 10);
